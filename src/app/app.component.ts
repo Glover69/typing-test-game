1bbe2d932ea77deb0044b603fbda3306cc38fa63
Glover69 <iamglover02@gmail.com>
2025-06-06 20:39:46 +0000

    an addition to the previous commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements AfterViewInit, OnDestroy {
+export class AppComponent {
   toastMessage: string = '';
   toastHeader: string = '';
   selectedTheme!: AppTheme; // This will hold the theme class name
@@ -39,14 +39,13 @@ export class AppComponent implements AfterViewInit, OnDestroy {
 
 
   ngOnInit(): void{
-
-    this.webSocketConnections();
+    this.animateThis();
 
     // Subscribe to theme changes
 
     this.themeSubscription = this.themeService.currentThemeClassName$.subscribe(themeClass => {
       this.selectedTheme = themeClass;
-      console.log('Current theme class in AppComponent:', this.selectedTheme);
+      // console.log('Current theme class in AppComponent:', this.selectedTheme);
     });
 
     this.toastService.toast$.subscribe((toast: Toast) => {
@@ -65,189 +64,25 @@ export class AppComponent implements AfterViewInit, OnDestroy {
     this.themeService.setTheme(themeClassName);
   }
 
-  ngAfterViewInit(): void {
-    const textElement = document.querySelector('.main-txt');
-    const buttonHolder = document.querySelector('.button-holder');
-    const animationContainer = document.querySelector('.entire-component') as HTMLElement; // Your main section
+  animateThis(){
+    const element = document.querySelector('.animate-this')
 
-
-    if (textElement) {
-      const originalText = 'SpeedType';
-      console.log(originalText)
-      textElement.textContent = ''; // Clear original text
-
-      // Split text into characters and wrap each in a span
-      originalText.split('').forEach(char => {
-        const charSpan = document.createElement('span');
-        charSpan.textContent = char;
-        charSpan.style.display = 'inline-block'; // Necessary for individual transforms if needed
-        charSpan.style.opacity = '0'; // Start hidden
-        textElement.appendChild(charSpan);
-      });
-
-      // Animate characters one by one
-      gsap.to(textElement.children, {
-        opacity: 1,
-        y: 0, // Optional: if you want them to slide in from a y-offset
-        stagger: 0.07, // Time between each character animation
-        duration: 0.5, // Duration of each character's animation
-        ease: 'power3.out',
-        delay: 1, // Initial delay before the animation starts
-        afterComplete: () => {
-          // After all characters are animated, you can do something here if needed
-          console.log('Text animation completed');
-          // this.textAnimationComplete = true; // Set flag to true when animation is complete
-        }
-      });
-    }
-
-    gsap.fromTo(buttonHolder, {
-      y: 50,
-      opacity: 0
+    gsap.fromTo(element, {
+      opacity: 0,
+      y: 70
     }, {
-      y: 0,
       opacity: 1,
-      ease: 'power4',
+      y: 0,
+      ease: 'power3.out',
       duration: 1.5,
-      delay: 2 // Delay to sync with text animation
-    });
-
-    // Create and animate meteors
-    if (animationContainer) {
-      const numMeteors = 150; // Adjust number of meteors
-      const containerWidth = animationContainer.offsetWidth;
-      const containerHeight = animationContainer.offsetHeight;
-
-      console.log('Container dimensions:', containerWidth, containerHeight);
-
-      for (let i = 0; i < numMeteors; i++) {
-        const meteor = document.createElement('div');
-        meteor.style.position = 'absolute';
-        meteor.style.width = `${Math.random() * 3 + 1}px`; // Random width (1px to 4px)
-        meteor.style.height = `32px`; // Square meteors
-        meteor.style.backgroundColor = 'var(--color-accent)'; // Use theme accent or fallback
-        meteor.style.borderRadius = '50%';
-        meteor.style.transformOrigin = 'center center'; // Ensure rotation is around the center
-        meteor.style.rotate = `${Math.random() * 360}deg`; // Random rotation
-        meteor.style.opacity = '0'; // Start invisible
-        animationContainer.appendChild(meteor);
-
-        // Function to set random animation properties
-        const animateMeteor = () => {
-          // Random start (edges) and end positions
-          const startX = Math.random() * containerWidth;
-          const startY = Math.random() < 0.5 ? -20 : containerHeight + 20; // Start from top or bottom edge
-          const endX = Math.random() * containerWidth;
-          const endY = startY > 0 ? -20 : containerHeight + 20; // End at opposite edge
-
-          gsap.fromTo(meteor,
-            { x: startX, y: startY, opacity: 0, scale: Math.random() * 0.4 + 0.6 },
-            {
-              x: endX,
-              y: endY,
-              opacity: Math.random() * 0.3 + 0.7, // Random opacity (0.3 to 0.8)
-              scale: Math.random() * 0.3 + 0.8,
-              duration: Math.random() * 5 + 4, // Random duration (3s to 8s)
-              delay: Math.random() * 6, // Random initial delay
-              ease: 'linear',
-              onComplete: animateMeteor, // Loop the animation by calling itself
-            }
-          );
-        };
-        animateMeteor(); // Start the animation for this meteor
+      delay: 3.5,
+      onComplete: () => {
+        console.log('Animation complete bottom');
+        // You can perform any additional actions here after the animation completes
       }
-    }
+    });
   }
 
 
-  webSocketConnections(){
-    // Web socket connections
-    this.subscriptions.add(
-      this.lobbyService.onLobbyCreated().subscribe(
-        (lobby) => {
-        console.log('Component: Lobby created with code:', lobby.code);
-        this.currentLobby = lobby;
-        this.toastService.showToast('Lobby Created', `Your lobby code was created successfully!`);
-        }
-    ));
 
-    this.subscriptions.add(
-      this.lobbyService.onJoinedLobby().subscribe(
-        (lobbyData) => {
-          console.log('Component: Successfully joined lobby:', lobbyData);
-          this.currentLobby = lobbyData;
-          this.toastService.showToast('Lobby Joined', `You joined lobby ${this.currentLobby.code} succesfully!`);
-        }
-      )
-    );
-
-    this.subscriptions.add(
-      this.lobbyService.onPlayerJoined().subscribe(
-        (playerJoinInfo) => {
-          console.log('Component: A new player joined:', playerJoinInfo);
-          this.toastService.showToast('New Player!', `${playerJoinInfo.playerName} has joined the lobby!`);
-
-          if (this.currentLobby) {
-            // Update the player list in the current lobby
-            this.currentLobby.players = playerJoinInfo.players;
-          }
-          // Update UI
-        }
-      )
-    );
-
-    this.subscriptions.add(
-      this.lobbyService.onLobbyError().subscribe(
-        (errorMsg) => {
-          // this.errorMessage = errorMsg;
-          console.error('Component: Lobby error:', errorMsg);
-          // Display error to user
-        }
-      )
-    );
-  }
-
-  toggleMultiplayerDialog(){
-    this.isMultiplayerMode = !this.isMultiplayerMode
-  }
-
-  toggleMultiplayerStatus(status: 'join-game' | 'create-game') {
-    this.multiplayerStatus = status;
-    this.lobbyCodeForJoining = ''; // Reset lobby code for joining
-    this.playerName = ''; // Reset player name
-    this.lobbyCodeForJoining = ''; // Reset lobby code after creating a lobby
-  }
-
-
-  createLobby() {
-    if (this.playerName.trim()) {
-      this.lobbyService.createLobby(this.playerName.trim());
-    } else {
-      console.log("Please enter a player name.");
-    }
-  }
-
-  joinLobby(){
-    const data = {
-      code: this.lobbyCodeForJoining.trim(),
-      playerName: this.playerName.trim()
-    }
-
-    if(data.playerName && data.code){
-      console.log('Joining lobby with data: ', data);
-      this.lobbyService.joinLobby(data);
-    }else{
-      console.log("misssing player name or lobby code");
-    }
-
-  }
-
-  ngOnDestroy(): void {
-    if (this.themeSubscription) {
-      this.themeSubscription.unsubscribe();
-    }
-
-    this.subscriptions.unsubscribe(); // Unsubscribe from all subscriptions
-
-  }
 }
